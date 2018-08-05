f2cf0dff4445c43c6c7422d8ef407d15cd022589
Pam De Silva <pam.desilva@gmail.com>
2018-08-05 19:12:42 +0100

    Setup dotenv to store API key

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 
 const config = {
-  apiKey: "AIzaSyA0pZ4Ngi1-Cjupbev1NolUX8lUkSGwv_Y",
-    authDomain: "yogi-finder.firebaseapp.com",
-    databaseURL: "https://yogi-finder.firebaseio.com",
-    projectId: "yogi-finder",
-    storageBucket: "yogi-finder.appspot.com",
-    messagingSenderId: "345283688925",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 };
 
 if (!firebase.apps.length) {
